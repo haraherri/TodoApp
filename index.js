3d789e85e8dac8b322a38218b401ff17a85165a4
haraherri <giahuy280703@gmail.com>
2024-09-09 15:48:58 +0700

    add first ui

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const PORT = 8000;
 const app = express();
 
+const connectionUrl = "mongodb://localhost:27017/todoDB";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("Database Connection Succesfully!"))
+  .catch((error) => console.log(error));
+
 app.set("view engine", "ejs");
 
+app.get("/", (req, res, next) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
